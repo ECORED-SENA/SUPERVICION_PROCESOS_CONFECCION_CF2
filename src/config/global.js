489e449b845f65574c0438482fb4e2c94f72b1d7
ricardog07 <71521082+ricardog07@users.noreply.github.com>
2021-08-06 16:03:27 -0500

    Update global.js

--- src/config/global.js
+++ src/config/global.js
@@ -153,6 +153,11 @@ export default {
         nombreRuta: 'referencias',
       },
       {
+        icono: 'fas fa-download',
+        titulo: 'Descargar material',
+        download: 'downloads/material.zip',
+      },
+      {
         icono: 'far fa-registered',
         titulo: 'Créditos',
         nombreRuta: 'creditos',
